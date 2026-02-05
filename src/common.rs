6e5fbd00c50da3cc29d2dbfd05264263e877ef3b
wdxqiang <1198901609@qq.com>
2026-02-05 13:55:08 +0800

    fix: 将默认API服务器地址更改为本地测试地址

--- src/common.rs
+++ src/common.rs
@@ -1081,7 +1081,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://116.62.55.163:21114/".to_owned()
 }
 
 #[inline]

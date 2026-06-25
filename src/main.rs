0878125f9866a76de4a756a6f6f303c04948fc94
wdxqiang <153586677+wdxqiang@users.noreply.github.com>
2026-06-25 13:58:38 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -48,7 +48,7 @@ fn main() {
     );
     let matches = App::new("rustdesk")
         .version(crate::VERSION)
-        .author("肖晨晰")
+        .author("wdxqiang")
         .about("RustDesk command line tool")
         .args_from_usage(&args)
         .get_matches();
